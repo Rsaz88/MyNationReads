d75019febe932a274c6af7ac32c69209577e4841
Raghad Saeed <61114699+Rsaz88@users.noreply.github.com>
2020-08-12 18:21:24 +0300

    Add files via upload

---
+++ php/header.php
@@ -0,0 +1,7 @@
+<?php
+echo '<a href="../html/Home.html">Home<a> -
+<a href="../html/Home.html">Home<a> -
+<a href="../html/NewBook.html">Add Book</a> -
+<a href="../html/MyLibrary.html">My Library</a> -
+<a href="../html/MyAccount.html">My Account<</a>';
+?>
